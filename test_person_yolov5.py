74527a47a2198a5a8fa1e4aba417cf78a81eabaf
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-06-17 12:40:07 +0700

    Add files via upload

--- test_person_yolov5.py
+++ test_person_yolov5.py
@@ -13,20 +13,20 @@ while True:
     _,frame = cap.read()
     frame = cv2.resize(frame,(640,360))
 
-    # results = model(frame, size=640)
-    # out2 = results.pandas().xyxy[0]
-    #
-    # if len(out2) != 0:
-    #     for i in range(len(out2)):
-    #         output_landmark = []
-    #         obj_name = out2.iat[i, 6]
-    #         if obj_name == 'person' or obj_name == '0':
-    #             xmin = int(out2.iat[i, 0])
-    #             ymin = int(out2.iat[i, 1])
-    #             xmax = int(out2.iat[i, 2])
-    #             ymax = int(out2.iat[i, 3])
-    #
-    #             cv2.rectangle(frame,(xmin,ymin),(xmax,ymax),(0,255,0),2)
+    results = model(frame, size=640)
+    out2 = results.pandas().xyxy[0]
+
+    if len(out2) != 0:
+        for i in range(len(out2)):
+            output_landmark = []
+            obj_name = out2.iat[i, 6]
+            if obj_name == 'person' or obj_name == '0':
+                xmin = int(out2.iat[i, 0])
+                ymin = int(out2.iat[i, 1])
+                xmax = int(out2.iat[i, 2])
+                ymax = int(out2.iat[i, 3])
+
+                cv2.rectangle(frame,(xmin,ymin),(xmax,ymax),(0,255,0),2)
     cv2.imshow('f',frame)
     k = cv2.waitKey(1)
     if k == ord('q'):
